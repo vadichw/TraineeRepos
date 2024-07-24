c644230098d2893c0d7c9e19a55697a32e4cea7f
vadichw <vadik56780098@gmail.com>
2024-07-24 21:43:27 +0300

    add

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def greeting(name):
-    print(f"Hello, my Lord {name}")
-    print(f"The best tester")
+import requests
+
+def check_security_headers(url):
+    response = requests.get(url)
+    security_headers = [
+        "Content-Security-Policy",
+        "X-Content-Type-Options",
+        "X-Frame-Options",
+        "Strict-Transport-Security",
+        "X-XSS-Protection"
+    ]
     
+    missing_headers = []
+    for header in security_headers:
+        if header not in response.headers:
+            missing_headers.append(header)
     
-greeting("Sauron")
+    if missing_headers:
+        print(f"Отсутствуют заголовки безопасности: {', '.join(missing_headers)}")
+    else:
+        print("Все основные заголовки безопасности присутствуют.")
+
+# Использование
+url = "https://www.github.com/"
+check_security_headers(url)
+
